1d1658c0badd40f31dcc1108d3906d1e54c06ed3
qifuong <94111065+qifuong@users.noreply.github.com>
2023-11-13 13:56:55 +0700

    Fix Lỗi
    AdminVerificationPage và ProfilePage

--- lib/Pages/AddNewBook/AdminVerificationPage.dart
+++ lib/Pages/AddNewBook/AdminVerificationPage.dart
@@ -9,209 +9,151 @@ class AdminVerificationPage extends StatefulWidget {
 }
 
 class _AdminVerificationPageState extends State<AdminVerificationPage> {
-  // Mật khẩu admin để kiểm tra
   String adminPassword = "123456";
-
-  // Controller để quản lý nội dung nhập vào ô "Tên Admin"
   TextEditingController passwordController = TextEditingController();
-
-  // Thông báo lỗi và thành công
   String errorMessage = "";
   String successMessage = "";
-
-  // Thông báo đặc biệt cho admin
-  String adminMessage = "";
-
-  // Số lần nhập sai mật khẩu
   int wrongPasswordCount = 0;
-
-  // Trạng thái đóng băng xác minh
   bool isVerificationFrozen = false;
-
-  // Controller cho ô mật khẩu trong popup
   TextEditingController popupPasswordController = TextEditingController();
-
-  // Thông báo lỗi trong popup
   String popupErrorMessage = "";
-
-  // Số lần nhập sai mật khẩu trong popup
   int wrongPopupPasswordCount = 0;
-
-  // Trạng thái đóng băng xác minh trong popup
   bool isPopupVerificationFrozen = false;
-
-  // Biến tạm để lưu giữ tên admin khi người dùng nhập vào ô "Tên Admin"
   String temporaryAdminName = "";
 
   @override
   void initState() {
     super.initState();
-    // Xóa giá trị trong ô "Tên Admin" khi trang được khởi tạo
     passwordController.clear();
   }
 
-  // Hàm xác minh admin
   void verifyAdmin() {
-    // Lưu giữ tên admin từ ô "Tên Admin"
     temporaryAdminName = passwordController.text;
 
-    // Kiểm tra xem xác minh có đang đóng băng không
     if (isVerificationFrozen) {
       return;
     }
 
-    // Lấy giá trị từ ô "Tên Admin"
     String userInput = temporaryAdminName;
 
-    // Kiểm tra xem tên admin có đúng không
+    if (userInput.isEmpty) {
+      errorMessage = "Vui lòng nhập tên Admin.";
+      setState(() {});
+      return;
+    }
+
     if (userInput == "Hung") {
       Navigator.pop(context);
     } else if (userInput == "Phuong") {
-      // Xử lý khi nhập sai mật khẩu quá 5 lần
-      if (wrongPasswordCount >= 5) {
-        errorMessage =
-        "Bạn đã nhập sai mật khẩu quá nhiều lần. Chức năng đã bị đóng băng.";
-        setState(() {});
-        Future.delayed(const Duration(seconds: 5), () {
-          errorMessage = "";
-          wrongPasswordCount = 0;
-          isVerificationFrozen = false;
-          setState(() {});
-          Navigator.popUntil(
-            context,
-            ModalRoute.withName(''), // Quay lại trang gốc
-          );
-        });
-        return;
-      }
-
-      // Thiết lập giá trị ban đầu cho ô mật khẩu trong popup
       popupPasswordController.text = "";
 
-      // Hiển thị hộp thoại xác minh
       showDialog(
         context: context,
         builder: (context) {
           return StatefulBuilder(
             builder: (BuildContext context, setState) {
-              return AlertDialog(
-                title: const Text("Xác minh Admin"),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text("Vui lòng nhập mật khẩu cho admin:"),
-                    TextField(
-                      controller: popupPasswordController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        labelText: "Mật khẩu",
-                        border: OutlineInputBorder(),
+              return WillPopScope(
+                onWillPop: () async {
+                  return !isPopupVerificationFrozen;
+                },
+                child: AlertDialog(
+                  title: const Text("Xác minh Admin"),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text("Vui lòng nhập mật khẩu cho admin:"),
+                      TextField(
+                        controller: popupPasswordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: "Mật khẩu",
+                          border: OutlineInputBorder(),
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      popupErrorMessage,
-                      style: const TextStyle(color: Colors.red),
-                    ),
-                    Text(
-                      adminMessage,
-                      style: const TextStyle(color: Colors.green),
+                      const SizedBox(height: 10),
+                      Text(
+                        popupErrorMessage,
+                        style: const TextStyle(color: Colors.red),
+                      ),
+                    ],
+                  ),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        if (isPopupVerificationFrozen) {
+                          popupErrorMessage =
+                          "Chức năng đã bị đóng băng. Vui lòng thử lại sau.";
+                          setState(() {});
+                          return;
+                        }
+
+                        if (popupPasswordController.text == adminPassword) {
+                          successMessage = "Xác minh thành công!";
+                          popupErrorMessage = "";
+                          errorMessage = "";
+                          setState(() {});
+
+                          Future.delayed(const Duration(seconds: 3)).then((_) {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const AddNewBook(),
+                              ),
+                            );
+
+                            popupPasswordController.clear();
+                            successMessage = "";
+                            setState(() {});
+                          });
+                        } else {
+                          popupErrorMessage =
+                          "Mật khẩu không đúng. Vui lòng thử lại.";
+                          wrongPopupPasswordCount++;
+                          setState(() {});
+
+                          if (wrongPopupPasswordCount == 3) {
+                            popupErrorMessage =
+                            "Bạn đã nhập sai mật khẩu 3 lần.";
+                            setState(() {});
+                          }
+
+                          if (wrongPopupPasswordCount == 5) {
+                            popupErrorMessage =
+                            "Bạn đã nhập sai mật khẩu 5 lần. Chức năng đã bị đóng băng.";
+                            isPopupVerificationFrozen = true;
+
+                            Future.delayed(const Duration(seconds: 5)).then((_) {
+                              Navigator.pop(context);
+                            });
+                          }
+                        }
+                      },
+                      child: const Text("Xác minh"),
                     ),
                   ],
                 ),
-                actions: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // Kiểm tra xem xác minh trong popup có đang đóng băng không
-                      if (isPopupVerificationFrozen) {
-                        popupErrorMessage =
-                        "Chức năng đã bị đóng băng. Vui lòng thử lại sau.";
-                        setState(() {});
-                        return;
-                      }
-
-                      // Xác minh mật khẩu admin
-                      if (popupPasswordController.text == adminPassword) {
-                        successMessage = "Xác minh thành công!";
-                        adminMessage = "Mật khẩu đúng!";
-                        popupErrorMessage = "";
-                        errorMessage = "";
-                        setState(() {});
-
-                        // Đợi 3 giây rồi chuyển đến trang thêm sách mới
-                        Future.delayed(const Duration(seconds: 3), () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const AddNewBook(),
-                            ),
-                          );
-
-                          // Xóa giá trị trong ô mật khẩu popup
-                          popupPasswordController.clear();
-                          successMessage = "";
-                          adminMessage = "";
-                          setState(() {});
-                        });
-                      } else {
-                        // Xử lý khi nhập sai mật khẩu trong popup
-                        popupErrorMessage =
-                        "Mật khẩu không đúng. Vui lòng thử lại.";
-                        adminMessage = "";
-                        wrongPopupPasswordCount++;
-                        setState(() {});
-
-                        // Hiển thị cảnh báo khi nhập sai 3 lần
-                        if (wrongPopupPasswordCount == 3) {
-                          popupErrorMessage =
-                          "Bạn đã nhập sai mật khẩu 3 lần.";
-                          setState(() {});
-                        }
-
-                        // Đóng băng xác minh trong popup khi nhập sai 5 lần
-                        if (wrongPopupPasswordCount == 5) {
-                          popupErrorMessage =
-                          "Bạn đã nhập sai mật khẩu 5 lần. Chức năng đã bị đóng băng.";
-                          isPopupVerificationFrozen = true;
-                          setState(() {});
-                        }
-                      }
-                    },
-                    child: const Text("Xác minh"),
-                  ),
-                ],
               );
             },
           );
         },
       );
     } else {
-      // Xử lý khi tên admin không đúng
       errorMessage = "Tên Admin không đúng. Vui lòng thử lại.";
-      adminMessage = "";
       wrongPasswordCount++;
       setState(() {});
 
-      // Hiển thị cảnh báo khi nhập sai 3 lần
       if (wrongPasswordCount == 3) {
         errorMessage = "Cảnh Báo !!! Bạn đã nhập sai mật khẩu 3 lần.";
         setState(() {});
       }
 
-      // Đóng băng xác minh khi nhập sai 5 lần
       if (wrongPasswordCount == 5) {
         errorMessage =
         "Bạn đã nhập sai mật khẩu 5 lần. Chức năng đã bị đóng băng.";
         isVerificationFrozen = true;
-        Future.delayed(const Duration(seconds: 5), () {
-          errorMessage = "";
-          wrongPasswordCount = 0;
-          isVerificationFrozen = false;
-          setState(() {});
-          Navigator.popUntil(
-            context,
-            ModalRoute.withName(''), // Quay lại trang gốc
-          );
+
+        Future.delayed(const Duration(seconds: 5)).then((_) {
+          Navigator.pop(context);
         });
       }
     }
@@ -219,9 +161,8 @@ class _AdminVerificationPageState extends State<AdminVerificationPage> {
 
   @override
   void dispose() {
-    // Xóa giá trị trong ô "Tên Admin" khi trang bị hủy
-    passwordController.clear();
-    temporaryAdminName = "";
+    passwordController.dispose();
+    popupPasswordController.dispose();
     super.dispose();
   }
 
@@ -229,8 +170,7 @@ class _AdminVerificationPageState extends State<AdminVerificationPage> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
-        setState(() {});
-        return true;
+        return !isVerificationFrozen;
       },
       child: Scaffold(
         appBar: AppBar(
@@ -268,10 +208,6 @@ class _AdminVerificationPageState extends State<AdminVerificationPage> {
                 Text(
                   errorMessage,
                   style: const TextStyle(color: Colors.red),
-                ),
-                Text(
-                  successMessage,
-                  style: const TextStyle(color: Colors.green),
                 ),
               ],
             ),
--- lib/Pages/ProfilePage/ProfilePage.dart
+++ lib/Pages/ProfilePage/ProfilePage.dart
@@ -1,25 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:e_book/Components/BackButton.dart';
 import 'package:e_book/Components/BookTile.dart';
 import 'package:e_book/Controller/AuthController.dart';
 import 'package:e_book/Controller/BookController.dart';
 import 'package:e_book/Models/Data.dart';
 import 'package:e_book/Pages/AddNewBook/AdminVerificationPage.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import '../BookDetails/BookDetails.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // Khởi tạo controller cho xác thực và sách
     AuthController authController = Get.put(AuthController());
     BookController bookController = Get.put(BookController());
 
     return Scaffold(
-      // Nút thêm sách mới
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Get.to(AdminVerificationPage());
@@ -29,11 +26,9 @@ class ProfilePage extends StatelessWidget {
           color: Theme.of(context).colorScheme.background,
         ),
       ),
-      // Nội dung chính của trang
       body: SingleChildScrollView(
         child: Column(
           children: [
-            // Phần đầu trang với thông tin cá nhân
             Container(
               padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
               color: Theme.of(context).colorScheme.primary,
@@ -45,7 +40,6 @@ class ProfilePage extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         SizedBox(height: 20),
-                        // Dòng tiêu đề và nút quay lại
                         Row(
                           crossAxisAlignment: CrossAxisAlignment.center,
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -63,45 +57,45 @@ class ProfilePage extends StatelessWidget {
                               ),
                             ),
                             IconButton(
-                                onPressed: () {
-                                  authController.signout();
-                                },
-                                icon: Icon(
-                                  Icons.exit_to_app,
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .background,
-                                ))
-                          ],
-                        ),
-                        SizedBox(height: 60),
-                        // Avatar người dùng
-                        Container(
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(100),
-                              border: Border.all(
-                                width: 2,
+                              onPressed: () {
+                                authController.signout();
+                              },
+                              icon: Icon(
+                                Icons.exit_to_app,
                                 color: Theme.of(context)
                                     .colorScheme
                                     .background,
-                              )),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 60),
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            border: Border.all(
+                              width: 2,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .background,
+                            ),
+                          ),
                           child: Container(
                             width: 120,
                             height: 120,
                             child: ClipRRect(
                               borderRadius: BorderRadius.circular(100),
                               child: Image.network(
-                                "${authController.auth.currentUser!.photoURL}",
+                                "${authController.auth.currentUser?.photoURL ?? ''}",
                                 fit: BoxFit.cover,
                               ),
                             ),
                           ),
                         ),
                         SizedBox(height: 20),
-                        // Tên và email người dùng
                         Text(
-                          "${authController.auth.currentUser!.displayName}",
+                          "${authController.auth.currentUser?.displayName ?? ''}",
                           style: Theme.of(context)
                               .textTheme
                               .bodyLarge
@@ -112,7 +106,7 @@ class ProfilePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          "${authController.auth.currentUser!.email}",
+                          "${authController.auth.currentUser?.email ?? ''}",
                           style: Theme.of(context)
                               .textTheme
                               .labelMedium
@@ -128,7 +122,6 @@ class ProfilePage extends StatelessWidget {
                 ],
               ),
             ),
-            // Phần sách đã đăng
             Padding(
               padding: const EdgeInsets.all(10),
               child: Column(
@@ -140,7 +133,6 @@ class ProfilePage extends StatelessWidget {
                     ],
                   ),
                   SizedBox(height: 10),
-                  // Danh sách sách đã đăng của người dùng
                   Column(
                     children: bookData
                         .map(
